85e0b1702ff0a5dc19ebc67d7dcb3aed7f8ed499
Josephjohn25 <johnjosephchibuikem98@gmail.com>
2023-11-13 05:36:04 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,30 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: Pointer to a string containing a binary number.
  *
- * Return: unsigned int with decimal value of binsry number, or 0 if error
+ * Return: The unsigned integer representation of the binary number,
+ *         or 0 if there is an error (invalid input).
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-if (b == NULL)
-return (0);
-unsigned int result = 0;
-while (*b)
-{
-if (*b != '0' && *b != '1')
-return (0);
-result = (result << 1) | (*b - '0');
-b++;
-}
-return (result);
-}
+int a;
+unsigned int num;
 
-int main(void)
-{
-const char *binaryString = "1101";
-unsigned int result = binary_to_uint(binaryString);
-printf("The decimal equivalent of %s is %u\n", binaryString, result);
-binaryString = "10102";
-result = binary_to_uint(binaryString);
-if (result == 0)
-{
-printf("%s is not a valid binary string\n", binaryString);
-}
+num = 0;
+if (!b)
 return (0);
+for (a = 0; b[a] != '\0'; a++)
+{
+if (b[a] != '0' && b[a] != '1')
+return (0);
+}
+for (a = 0; b[a] != '\0'; a++)
+{
+num <<= 1;
+if (b[a] == '1')
+num += 1;
+}
+return (num);
 }
